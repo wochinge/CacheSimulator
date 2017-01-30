b63f93e7753ebc05932c3f231aa7fd30851544e7
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-01-30 17:04:37 +0100

    improved naming

--- src/SimpleCaches.hs
+++ src/SimpleCaches.hs
@@ -6,6 +6,6 @@ import Cache
 
 readFromCache' :: Cache a => (File -> a -> (Bool, a)) -> File -> a -> (Bool, a)
 readFromCache' updateCache file cache =
-    let (wasUpdated, updatedCache) = updateCache file cache
-        newCache = if not wasUpdated then file `to` cache else updatedCache
-    in (wasUpdated, newCache)
+    let (wasInCache, updatedCache) = updateCache file cache
+        newCache = if not wasInCache then file `to` cache else updatedCache
+    in (wasInCache, newCache)
